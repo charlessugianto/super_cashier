f9d99ad4aaa57cf4f97e14062fa29f2341d1a63a
Charles Sugianto <129651972+charlessugianto@users.noreply.github.com>
2023-04-12 20:27:19 +0700

    Add files via upload

---
+++ ProjectSuperCashier.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import SuperCashier1 as s
+
+
+# In[2]:
+
+
+# Test Case 1: Menambahkan dua item baru
+user = s.Transaction()
+print("Test Case 1")
+user.add_item("Ayam Goreng", 2, 20_000)
+print("\n")
+user.add_item("Pasta Gigi", 3, 15_000)
+print("\n")
+print("Berikut adalah transaksi belanja Anda :")
+user.check_order()
+user.total_price()
+
+
+# In[3]:
+
+
+# Test Case 2: Menghapus salah satu item
+user.delete_item("Pasta Gigi")
+print("Berikut adalah transaksi belanja Anda :")
+user.check_order()
+user.total_price()
+
+
+# In[4]:
+
+
+# Test Case 3: Menghapus semua item
+user.reset_transaction()
+
+
+# In[5]:
+
+
+# Test Case 4: Menghitung total belanja
+user.add_item("Ayam Goreng", 2, 20_000)
+print("\n")
+user.add_item("Pasta Gigi", 3, 15_000)
+print("\n")
+user.add_item("Mainan Mobil", 1, 200_000)
+print("\n")
+user.add_item("Mie Instan", 5, 3_000)
+print("\n")
+print("Tabel Pemesanan :")
+user.check_order()
+user.total_price()
+
+
+# In[6]:
+
+
+# Test Case 5: Memperbaharui Item, Quantity, dan Harga
+print("Tabel Pemesanan Sebelum Update:")
+user.check_order()
+user.total_price()
+print("\n")
+# Update Item pasta gigi
+user.update_item_name("Pasta Gigi", "Sabun Mandi")
+# Update Quantity ayam goreng
+user.update_item_quantity("Ayam Goreng", 3)
+# Update Harga mie instan
+user.update_item_price("Mie Instan", 3_500)
+print("\n")
+print("Tabel Pemesanan Setelah Update:")
+user.check_order()
+user.total_price()
+
+
+# In[7]:
+
+
+# Test Case 6: Kesalahan Input
+user.add_item("Gula", -25, 10_000)
+
+
+# In[8]:
+
+
+# Test Case 7: Kesalahan Input
+user.add_item("Gula", "Lima", 10_000)
+
+
+# In[ ]:
+
+
+
+
